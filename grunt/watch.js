c016d6d409ef0c6717bc4cfaa516aad597948634
Rhys Evans <wheresrhys@gmail.com>
2014-02-23 12:23:36 +0000

    watch

--- grunt/watch.js
+++ grunt/watch.js
@@ -2,6 +2,10 @@ module.exports = {
     templates: {
         files: 'templates/**/*.html',
         tasks: ['concat:dev']
+    },
+    style: {
+        files: 'style/**/*.scss',
+        tasks: ['sass:dev']
     } //,
     // styluses: {
     //   files: 'style/components/*.styl',
